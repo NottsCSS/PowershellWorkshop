8a7b1f7cba05686855beb61c977146f4c1ad452c
ks <leekashing2107@gmail.com>
2019-04-09 21:09:06 +0800

    fixed searchOpen.ps1

--- Lesson2/searchOpen.ps1
+++ Lesson2/searchOpen.ps1
@@ -11,16 +11,17 @@
     search(C, hey.txt)
 
 #>
+	   
 
-function search{
-    param($drive,$targetFile)
-    
-    $item = Get-ChildItem -Path $drive -Recurse |
-            Select-Object -ExpandProperty FullName |
-            Where-Object -Property {$_.BaseName -eq $targetFile } 
+function search ($targetFile) {
+	write-host $targetFile
+	$item = Get-ChildItem . -recurse |
+            Where-Object {$_.Name -eq $targetFile} |
+			Select-Object -ExpandProperty FullName 
 
-    $path = $item.PSpath
-
-    explorer $path
-    
+	if ($item -ne $null) {
+		Invoke-Item $item
+	}
 }
+
+search($args[0])
