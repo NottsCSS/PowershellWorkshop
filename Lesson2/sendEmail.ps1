40baeb394b8510683493ec8cc33ce95d2acc4326
Lee Ka Shing <leekashing2107@gmail.com>
2019-04-11 00:08:43 +0800

    removed sendEmail.ps1

--- Lesson2/sendEmail.ps1
+++
@@ -1,7 +0,0 @@
-$From = ""
-$To = "hcykl4@nottingham.edu.my"
-$Subject = "Here's the Email Subject"
-$Body = "This is what I want to say"
-$SMTPServer = "smtp.live.com"
-$SMTPPort = "587"
-Send-MailMessage -From $From -to $To -Subject $Subject -Body $Body -SmtpServer $SMTPServer -port $SMTPPort -UseSsl -Credential (Get-Credential) –DeliveryNotificationOption OnSuccess
